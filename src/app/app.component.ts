56b35babaf2b50456232f580b516a6cef8f15920
Aniket Jadhav <jadhavaniket0118@gmail.com>
2023-12-12 17:35:28 +0530

    first comit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { HttpClient, HttpRequest, HttpEventType, HttpResponse } from '@angular/common/http'
+ import * as XLSX from 'xlsx'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'EXCEL-App';
+  excelData: any[];
+
+  onFileChange(event: any): void {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = (e: any) => {
+      const workbook = XLSX.read(e.target.result, { type: 'binary' });
+      const sheetName = workbook.SheetNames[0];
+      const sheet = workbook.Sheets[sheetName];
+      this.excelData = XLSX.utils.sheet_to_json(sheet, { header: 1 });
+    };
+
+    reader.readAsBinaryString(file);
+  }
 }
